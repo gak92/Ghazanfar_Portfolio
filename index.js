41c5ef56d90a6ce7d729fb625b4326cd0af0c7cc
Bahoz <bahozali50@gmail.com>
2022-06-13 18:53:20 +0300

    display the close button in the top right of the mobile menu

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const menu = document.getElementsByClassName("menu")[0];
-const navbar = document.getElementsByClassName("navbar-list")[0]
+const navbar = document.getElementsByClassName("navbar-list")[0];
+const closeButton = document.getElementsByClassName("bi-x")[0];
 
 menu.addEventListener("click", () => {
   navbar.classList.add("navbar--open");
-  document.body.style.overflow = "hidden"
+  document.body.style.overflow = "hidden";
+  closeButton.classList.add("x--open");
 });
