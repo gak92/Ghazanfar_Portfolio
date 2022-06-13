b26fdc234bb5b79a4517845b30a9dd79cf0adaa2
gak92 <44802922+gak92@users.noreply.github.com>
2022-06-14 04:38:57 +0500

    Correct linters error

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const menu = document.getElementsByClassName("menu")[0];
-const navbar = document.getElementsByClassName("navbar-list")[0];
-const closeButton = document.getElementsByClassName("bi-x")[0];
-const navItems = document.getElementsByClassName("navbar-item");
+const menu = document.getElementsByClassName('menu')[0];
+const navbar = document.getElementsByClassName('navbar-list')[0];
+const closeButton = document.getElementsByClassName('bi-x')[0];
+const navItems = document.getElementsByClassName('navbar-item');
 
-menu.addEventListener("click", () => {
-  navbar.classList.add("navbar--open");
-  document.body.style.overflow = "hidden";
-  closeButton.classList.add("x--open");
-  menu.classList.add("menu--close");
+menu.addEventListener('click', () => {
+  navbar.classList.add('navbar--open');
+  document.body.style.overflow = 'hidden';
+  closeButton.classList.add('x--open');
+  menu.classList.add('menu--close');
 });
 
-closeButton.addEventListener("click", closeMobileMenu);
-
-for (let navitem of navItems) {
-  navitem.addEventListener("click", closeMobileMenu);
-}
-
 function closeMobileMenu() {
-  navbar.classList.remove("navbar--open");
-  document.body.style.overflowY = "scroll";
-  closeButton.classList.remove("x--open");
-  menu.classList.remove("menu--close");
+  navbar.classList.remove('navbar--open');
+  document.body.style.overflowY = 'scroll';
+  closeButton.classList.remove('x--open');
+  menu.classList.remove('menu--close');
 }
+
+closeButton.addEventListener('click', closeMobileMenu);
+
+for (const navitem of navItems) {
+  navitem.addEventListener('click', closeMobileMenu);
+}
+
+// navItems.forEach(function close(navitem) {
+//   navitem.addEventListener('click', closeMobileMenu);
+// });
