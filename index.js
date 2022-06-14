17742eba92905eb6e80ae8906784686350fb83df
gak92 <44802922+gak92@users.noreply.github.com>
2022-06-15 03:00:34 +0500

    remove linters error

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function blur(headline, logo, number) {
+  headline.style.filter = `blur(${number}px)`;
+  logo.style.filter = `blur(${number}px)`;
+}
+
+if (typeof window !== 'undefined') {
   const menu = document.getElementsByClassName('menu')[0];
   const navbar = document.getElementsByClassName('navbar-list')[0];
   const closeButton = document.getElementsByClassName('bi-x')[0];
@@ -5,31 +11,29 @@
   const headline = document.getElementsByClassName('headline')[0];
   const logo = document.getElementsByClassName('logo')[0];
 
-function blur(number) {
-  headline.style.filter = `blur(${number}px)`;
-  logo.style.filter = `blur(${number}px)`;
-}
-
-function closeMobileMenu() {
-  navbar.classList.remove('navbar--open');
-  document.body.style.overflowY = 'scroll';
-  closeButton.classList.remove('x--open');
-  menu.classList.remove('menu--close');
-  blur(0);
-}
-
-if (typeof window !== 'undefined') {
   menu.addEventListener('click', () => {
     navbar.classList.add('navbar--open');
     document.body.style.overflow = 'hidden';
     closeButton.classList.add('x--open');
     menu.classList.add('menu--close');
-    blur(6);
+    blur(headline, logo, 6);
   });
 
-  closeButton.addEventListener('click', closeMobileMenu);
+  closeButton.addEventListener('click', () => {
+    navbar.classList.remove('navbar--open');
+    document.body.style.overflowY = 'scroll';
+    closeButton.classList.remove('x--open');
+    menu.classList.remove('menu--close');
+    blur(headline, logo, 0);
+  });
 
   Array.from(navItems).forEach((navitem) => {
-    navitem.addEventListener('click', closeMobileMenu);
+    navitem.addEventListener('click', () => {
+      navbar.classList.remove('navbar--open');
+      document.body.style.overflowY = 'scroll';
+      closeButton.classList.remove('x--open');
+      menu.classList.remove('menu--close');
+      blur(headline, logo, 0);
+    });
   });
 }
