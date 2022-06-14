9acd600227872c7611b6d5ec9360621091e6f767
gak92 <44802922+gak92@users.noreply.github.com>
2022-06-15 00:46:57 +0500

    Correct background color and blur effect, and tried to fix linters error as suggested but not succeed

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const menu = document.getElementsByClassName('menu')[0];
 const navbar = document.getElementsByClassName('navbar-list')[0];
 const closeButton = document.getElementsByClassName('bi-x')[0];
 const navItems = document.getElementsByClassName('navbar-item');
+const headline = document.getElementsByClassName('headline')[0];
+const logo = document.getElementsByClassName('logo')[0];
+
+function blur(number) {
+  headline.style.filter = `blur(${number}px)`;
+  logo.style.filter = `blur(${number}px)`;
+}
 
 menu.addEventListener('click', () => {
   navbar.classList.add('navbar--open');
   document.body.style.overflow = 'hidden';
   closeButton.classList.add('x--open');
   menu.classList.add('menu--close');
+  blur(6);
 });
 
 function closeMobileMenu() {
@@ -15,6 +23,7 @@ function closeMobileMenu() {
   document.body.style.overflowY = 'scroll';
   closeButton.classList.remove('x--open');
   menu.classList.remove('menu--close');
+  blur(0);
 }
 
 closeButton.addEventListener('click', closeMobileMenu);
