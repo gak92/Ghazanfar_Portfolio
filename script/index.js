125be06a037ed6a35d9948813776a828865ae1f0
gak92 <44802922+gak92@users.noreply.github.com>
2022-06-15 19:31:11 +0500

    Update index.js to clean the code and create function where necessary

--- script/index.js
+++ script/index.js
@@ -8,6 +8,7 @@ const seeBtn = document.getElementsByClassName('see_project')[0];
 const wrapper = document.getElementsByClassName('wrapper')[0];
 const closeproject = document.getElementById('close-project');
 
+// Function to blur the background
 function blur(headline, logo, number) {
   headline.style.filter = `blur(${number}px)`;
   logo.style.filter = `blur(${number}px)`;
@@ -21,41 +22,46 @@ function enableScroll() {
   document.body.style.overflowY = 'auto';
 }
 
-// if (typeof window !== 'undefined') {
-  
-  seeBtn.addEventListener('click', () => {
-    stopScroll();
-    wrapper.classList.add('wrapper--open');
-  });
+// Open Menu
+menu.addEventListener('click', () => {
+  navbar.classList.add('navbar--open');
+  document.body.style.overflow = 'hidden';
+  closeButton.classList.add('x--open');
+  menu.classList.add('menu--close');
+  blur(headline, logo, 6);
+});
 
-  closeproject.addEventListener('click', () => {
-    enableScroll();
-    wrapper.classList.remove('wrapper--open');
-  });
+// Function to close the Menu
+function closeMenu() {
+  navbar.classList.remove('navbar--open');
+  document.body.style.overflowY = 'scroll';
+  closeButton.classList.remove('x--open');
+  menu.classList.remove('menu--close');
+  blur(headline, logo, 0);
+}
 
-  menu.addEventListener('click', () => {
-    navbar.classList.add('navbar--open');
-    document.body.style.overflow = 'hidden';
-    closeButton.classList.add('x--open');
-    menu.classList.add('menu--close');
-    blur(headline, logo, 6);
-  });
+// Close Menu when click on Close Button
+closeButton.addEventListener('click', () => {
+  closeMenu();
+});
 
-  closeButton.addEventListener('click', () => {
-    navbar.classList.remove('navbar--open');
-    document.body.style.overflowY = 'scroll';
-    closeButton.classList.remove('x--open');
-    menu.classList.remove('menu--close');
-    blur(headline, logo, 0);
-  });
 
-  Array.from(navItems).forEach((navitem) => {
-    navitem.addEventListener('click', () => {
-      navbar.classList.remove('navbar--open');
-      document.body.style.overflowY = 'scroll';
-      closeButton.classList.remove('x--open');
-      menu.classList.remove('menu--close');
-      blur(headline, logo, 0);
-    });
+// Close Menu when click on Menu option
+Array.from(navItems).forEach((navitem) => {
+  navitem.addEventListener('click', () => {
+    closeMenu();
   });
-// }
+});
+
+// Open Project Pop up Window
+seeBtn.addEventListener('click', () => {
+  stopScroll();
+  wrapper.classList.add('wrapper--open');
+});
+
+// Close Project Pop up Window
+closeproject.addEventListener('click', () => {
+  enableScroll();
+  wrapper.classList.remove('wrapper--open');
+});
+
